c64bb08bf1274c658c4b0bb8a8ea5fa702db7bf2
hamtorililil <hojin8876@naver.com>
2022-04-11 17:50:05 +0900

    commit

---
+++ 웹디자인/웹사이트 제작/js/웹사이트 제작.js
@@ -0,0 +1,6 @@
+$(".nav>li").mouseover(function () { 
+    $(this).children(".submenu").stop().slideDown();
+});
+$(".nav>li").mouseleave(function () { 
+    $(this).children(".submenu").stop().slideUp();    
+});
