5f6724443f29daf5a2e446e568e49836f38f4ef5
hamtorililil <hojin8876@naver.com>
2022-05-16 15:22:21 +0900

    commit

--- 웹디자인/베이커리/js/베이커리.js
+++ 웹디자인/베이커리/js/베이커리.js
@@ -1,25 +1,40 @@
+// JavaScript Document
 
 var imgs = 4;
 var now = 0;
 
-$(document).ready(function () {
-    $(".nav>li").mouseover(function(){
-        $(this).children(".submenu").stop().slideDown();
-    });
-    $(".nav>li").mouseleave(function () { 
-        $(this).children(".submenu").stop().slideUp();
-    });
-
-    start();
+jQuery(document).ready(function() {
+   $(".nav>li").mouseover(function(){
+	$(this).children(".submenu").stop().slideDown();
+	});
+   $(".nav>li").mouseleave(function(){
+	$(this).children(".submenu").stop().slideUp();
+   });
+   
+   start(); 
+   
+   $(".partner img").click(function (){
+      $("#partner_up").addClass("active");
+   });
+   $("#partner_up button").click(function (){
+      $("#partner_up").removeClass("active");
+   });
+   
+   $(".notice tr").click(function (){
+       $("#modal").addClass("active");
+   });
+   $(".btn2").click(function(){ 
+       $("#modal").removeClass("active");
+   });
+   
 });
 
 function start(){
-    $(".imgs>img").eq(0).siblings().
-    css({"margin-top":"-800px"});
-    setInterval(function() {slide();},4000);
+   $(".imgs>img").eq(0).siblings().css({"margin-top":"-800px"});
+   setInterval(function(){slide();},4000);
 }
 function slide(){
-    now=now==imgs?0:now+=1;
-    $(".imgs>img").eq(now-1).css({"margin-top":"-800px"});
-    $(".imgs>img").eq(now).css({"margin-top":"0px"});
+   now = now==imgs?0:now+=1;
+   $(".imgs>img").eq(now-1).css({"margin-top":"-800px"});   
+   $(".imgs>img").eq(now).css({"margin-top":"0px"});
 }
