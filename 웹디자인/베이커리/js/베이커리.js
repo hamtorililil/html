6fc64088f1c5af3ca1f69b3345aa582024a0cdf3
hamtorililil <hojin8876@naver.com>
2022-05-16 10:47:53 +0900

    commit

--- 웹디자인/베이커리/js/베이커리.js
+++ 웹디자인/베이커리/js/베이커리.js
@@ -12,3 +12,14 @@ $(document).ready(function () {
 
     start();
 });
+
+function start(){
+    $(".imgs>img").eq(0).siblings().
+    css({"margin-top":"-800px"});
+    setInterval(function() {slide();},2000);
+}
+function slide(){
+    now=now==imgs?0:now+=1;
+    $(".imgs>img").eq(now-1).css({"margin-top":"-800px"});
+    $(".imgs>img").eq(now).css({"margin-top":"0px"});
+}
