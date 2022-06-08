4d6e77c84d9d84c41127a61ff69a1905a4b33803
hamtorililil <hojin8876@naver.com>
2022-06-08 12:41:29 +0900

    commit

--- 웹디자인/B-1/B-1_수험자제공파일/jQuery 오픈소스 파일/B-1.js
+++ 웹디자인/B-1/B-1_수험자제공파일/jQuery 오픈소스 파일/B-1.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
     }).mouseout(function(){
         $(".submenu").stop().slideUp();
     });
+
+    var index = 0, prev, imgList = $(".imglist li"), size = imgList.length;
+    setInterval(function(){ 
+        prev = index;   
+        index++;
+        if(index >= size) index = 0;
+        imgList.eq(index).css({left: 1200+'px'});
+        imgList.eq(index).animate({left: 0},1000);
+        imgList.eq(prev).animate({left : -1200+'px'},1000);
+    }, 3000);
 });
